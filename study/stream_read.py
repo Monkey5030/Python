4fe45a04f0bb23929832fd70ffa7fb99127d16a6
Monkey5030 <1565774885@qq.com>
2021-04-12 05:50:10 -0500

    Add files via upload

---
+++ study/stream_read.py
@@ -0,0 +1,57 @@
+#encoding=utf-8
+
+'''
+需要安装的包
+pymysql
+aiomysql
+'''
+import asyncio
+import aiomysql
+import pandas as pd
+import pymysql
+import time
+
+# data1=pd.DataFrame(columns=['area_code','area_name','level','parent_code'])
+# conn=pymysql.connect(host='127.0.0.1',user='root',password='popeye12.', db='work', charset='utf8')
+# data=data.append(pd.read_sql('select * from work.area_code_2021 limit 100',conn))
+# print(data)
+
+
+async def dbdaochu(loop):
+    # 生成一个空的DataFrame结构
+    dd=pd.DataFrame(data=[(None,None,None,None),],columns=['area_code','area_name','level','parent_code'])
+    sqlstr='select * from work.area_code_2021 limit 10'
+    conn = await aiomysql.connect(host='127.0.0.1', user='root',
+                           password='popeye12.', db='work', charset='utf8',
+                                  loop=loop)
+    async with aiomysql.cursors.SSCursor(conn) as cursor:
+        await cursor.execute(sqlstr)
+        while True:
+            # rowlist=[]
+            row = await cursor.fetchone()
+            if not row:
+                break
+            # rowlist.append(row)
+            # print(rowlist)
+            # 使用元组组成的列表构建DataFrame
+            df=pd.DataFrame(data=[row],columns=['area_code','area_name','level','parent_code'])
+            # print(df)
+            #合并两个DataFrame
+            dd=pd.concat([dd,df],ignore_index=True)
+        # print(dd)
+        return dd
+
+        # data=data1.append(row)
+    # return data1	
+
+            
+    
+if __name__ == '__main__':
+    start=time.time()
+    loop = asyncio.get_event_loop()
+    data2=loop.run_until_complete(dbdaochu(loop))
+    data2.drop(data2.index[0],inplace=True)
+    print(data2)
+    end=time.time()
+    print('此次执行共花费%s s'%(end-start))
+    loop.close()
