f83894700505a01dea3ef1dc3f8d635b515eebc5
Monkey5030 <1565774885@qq.com>
2021-06-10 16:40:16 +0800

    Update thread_many.py

--- study/thread_many.py
+++ study/thread_many.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 10 15:34:00 2021
+
+@author: Admin
+"""
+
 #encoding=utf-8
 
 from threading import Thread
@@ -5,14 +12,15 @@ from queue import Queue
 import requests
 from bs4 import BeautifulSoup
 import lxml
-import os
+import os,time,re
+from jieba.analyse import *
+from concurrent.futures import ThreadPoolExecutor
 
 class DownSpider(Thread):
     def __init__(self,queue,path):
         Thread.__init__(self)
         self.queue=queue
         self.path=path
-
 
     def run(self):
         while True:
@@ -29,9 +37,29 @@ def download_img(url,path):
     img_list=soup.find_all('img',class_='ui image lazy')
     for img in img_list:
         href=img.get('data-original')
-        title=img.get('title').split('/')[0].replace('?','')[:10]
+        title=img.get('title')
+#        正则替换特殊字符
+        strinfo = re.compile(r'[/:*?"<>|\\\(\)]')    # 加r,2个\即可
+        title = strinfo.sub('', title)
+        l1=list()
+#        提取关键词
+        for keyword, weight in textrank(title,topK=10, withWeight=True):
+#        for keyword, weight in extract_tags(title, withWeight=True):
+            l1.append(keyword)
+#        print(l1)
+        if l1:
+            title=l1[0]
+        else:
+            title
+
         try:
-            if os.path.splitext(href)[-1]!='.gif':
+#            如果存在同名文件，在文件名后追加时间戳，否则直接打开文件
+            img_path=path+title+os.path.splitext(href)[-1]
+            if os.path.exists(img_path):
+                with open(path+title+str(int(time.time()))+os.path.splitext(href)[-1],'wb') as f:
+                    img_content=requests.get(href).content
+                    f.write(img_content)
+            else:
                 with open(path+title+os.path.splitext(href)[-1],'wb') as f:
                     img_content=requests.get(href).content
                     f.write(img_content)
@@ -43,19 +71,23 @@ def download_img(url,path):
 
 if __name__=='__main__':
     url_list=[]
-    for i in range(1,200):
+    for i in range(1,10):
         url=f'https://fabiaoqing.com/biaoqing/lists/page/{i}.html'
         url_list.append(url)
     path='./表情包3/'
+    #线程池
+    with ThreadPoolExecutor() as p:   #类似打开文件,可省去.shutdown()
+        future_tasks = [p.submit(download_img, item,path) for item in url_list]
+    
+    [obj.result() for obj in future_tasks]
 
-    queue=Queue()
-
-
-    for thread in range(10):
-        work=DownSpider(queue,path)
-        work.daemon=True
-        work.start()
-
-    for item in url_list:
-        queue.put(item)
-    queue.join()
+#    队列
+#    queue=Queue()
+#    for thread in range(10):
+#        work=DownSpider(queue,path)
+#        work.daemon=True
+#        work.start()
+#
+#    for item in url_list:
+#        queue.put(item)
+#    queue.join()
